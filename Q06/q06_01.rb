264b63d3ff2c1beed65b930d90d40680403cd6ef
m-henmi <m.henmi@gmail.com>
2016-01-07 08:32:29 +0900

    Add file.
    * new file:   q06_01.rb

---
+++ Q06/q06_01.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+# coding: utf-8
+
+start = Time.now
+
+def is_loop(n)
+  check = n * 3 + 1
+
+  while check != 1 do
+    check = check.even? ? check / 2 : check * 3 + 1
+    return true if check == n
+  end
+
+  return false
+end
+
+puts 2.step(10000, 2).count{|i|
+  is_loop(i)
+}
+
+puts "[" + (Time.now - start).to_s + " sec.]"
+
+=begin
+
+=end
